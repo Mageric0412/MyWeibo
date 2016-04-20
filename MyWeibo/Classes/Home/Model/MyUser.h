fd6b836df95a2ae5c65192ec1402dfeed8acccea
Mageric <mageric0412@gmail.com>
2016-04-20 19:36:39 +0800

    NewStatus

---
+++ MyWeibo/Classes/Home/Model/MyUser.h
@@ -0,0 +1,21 @@
+//
+//  MyUser.h
+//  MyWeibo
+//
+//  Created by mageric-mac on 16/4/19.
+//  Copyright © 2016年 mageric-mac. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MyUser : NSObject
+/**
+ *  微博昵称
+ */
+@property (nonatomic, copy) NSString *name;
+/**
+ *  微博头像
+ */
+@property (nonatomic, strong) NSURL *profile_image_url;
+
+@end
---
+++ MyWeibo/Classes/Home/Model/MyUser.m
@@ -0,0 +1,13 @@
+//
+//  MyUser.m
+//  MyWeibo
+//
+//  Created by mageric-mac on 16/4/19.
+//  Copyright © 2016年 mageric-mac. All rights reserved.
+//
+
+#import "MyUser.h"
+
+@implementation MyUser
+
+@end
